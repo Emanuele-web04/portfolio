c388aa8f186523df989de0c2b70c87e25d2e2006
Emanuele-web04 <emanueledipietro612@gmail.com>
2025-04-21 03:06:50 +0200

    comments

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
-import React from "react";
+import React, { Suspense } from "react";
 import Navbar from "@/components/Navbar";
 import Apps from "@/components/Apps";
 const Home = () => {
   return (
     <div className="m-auto overflow-hidden">
       <Navbar/>
-      <Apps/>
+      <Suspense fallback={<div>Loading...</div>}>
+        <Apps />
+      </Suspense>
     </div>
   )
 };
