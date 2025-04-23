5e7b10cca4b6233c76a9aa554a323c41df7f76b6
Emanuele-web04 <emanueledipietro612@gmail.com>
2025-04-23 14:35:04 +0200

    comments

--- src/data/experience.ts
+++ src/data/experience.ts
@@ -5,7 +5,7 @@ export const experience: Project[] = [
         image: "/ada.jpeg",
         title: "Apple Developer Academy | Unina",
         description: "Student & Alumni at Apple Developer Academy",
-        release: "October 2024 - June 2024",
+        release: "October 2023 - June 2024",
         link: "https://www.developeracademy.unina.it/en/"
     },
 ]
